881ebdf95d508f1f6f0436c4c8da71bdaa8091ee
“ArenasAgustin” <arenasagustin7@gmail.com>
2022-01-04 15:26:10 -0300

    shadowa & star

--- src/components/Tree/Tree.js
+++ src/components/Tree/Tree.js
@@ -3,7 +3,10 @@ import "./Tree.scss";
 
 export default function Tree() {
   return (
-    <div className="tree-container">
+    <div className="container">
+      <div class="star-container">
+        <div class="star"></div>
+      </div>
       <div className="tree">
         <div className="leaves small"></div>
         <div className="leaves medium"></div>
