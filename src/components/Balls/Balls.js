dca33986580a7321959b122aac1a59d21184c3b6
“ArenasAgustin” <arenasagustin7@gmail.com>
2022-01-04 16:57:21 -0300

    balls

---
+++ src/components/Balls/Balls.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Balls.scss";
+
+export default function Balls() {
+  return (
+    <div class="balls-container">
+      <div class="balls-group">
+        <div class="ball"></div>
+        <div class="ball"></div>
+        <div class="ball"></div>
+        <div class="ball"></div>
+      </div>
+      <div class="balls-group">
+        <div class="ball"></div>
+        <div class="ball"></div>
+        <div class="ball"></div>
+        <div class="ball"></div>
+      </div>
+      <div class="balls-group">
+        <div class="ball"></div>
+        <div class="ball"></div>
+        <div class="ball"></div>
+        <div class="ball"></div>
+        <div class="ball"></div>
+        <div class="ball"></div>
+      </div>
+    </div>
+  );
+}
--- src/components/Tree/Tree.js
+++ src/components/Tree/Tree.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Balls from "../Balls/Balls";
 import "./Tree.scss";
 
 export default function Tree() {
@@ -7,12 +8,15 @@ export default function Tree() {
       <div class="star-container">
         <div class="star"></div>
       </div>
+
       <div className="tree">
         <div className="leaves small"></div>
         <div className="leaves medium"></div>
         <div className="leaves large"></div>
         <div className="log"></div>
       </div>
+
+      <Balls />
     </div>
   );
 }
